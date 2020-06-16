425fcd307df5ecb1925a65f1edb0a8ee1107abf7
1094476088@qq.com <huangbangyi2019>
2020-06-16 15:18:57 +0800

    LQG任务更新2020年6月16日 15:18:42

---
+++ team-project/src/com/nnxy/team3/LuQingge.java
@@ -0,0 +1,20 @@
+package com.nnxy.team3;
+
+public class LuQingge {
+
+	private String name;
+	private Integer id;
+	private String clas;
+	
+	LuQingge(){
+		this.name="陆庆阁";
+		this.id=065;
+		this.clas="17物联网工程1班";
+	}
+	
+	public void name() {
+		System.out.println("我的名字是"+name);
+		System.out.println("我的学号是"+id);
+		System.out.println("我的班级是"+clas);
+	}
+}
