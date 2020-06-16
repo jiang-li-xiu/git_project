4df2d820e5e65ed6a8792e1ff9a70833c57fc0c3
huangbangyi2019 <1094476088@qq.com>
2020-06-16 16:54:27 +0800

    梁钧全提交-嘤嘤嘤

--- team-project/src/com/nnxy/team2/Liangjunquan.java
+++ team-project/src/com/nnxy/team2/Liangjunquan.java
@@ -1,5 +1,22 @@
 package com.nnxy.team2;
 
 public class Liangjunquan {
+	
+	/*Áº¾ûÈ«*/
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private int num;
+	private String name;
 
 }
